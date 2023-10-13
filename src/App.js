1c3654428d97e5c785214b22eea963b982846f5d
adiigpp <143987041+adiigpp@users.noreply.github.com>
2023-10-14 00:07:49 +0700

    Add files via upload
    maaf telat

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Intro from "./components/intro";
+import Navbar from "./components/navbar";
+import Aboutme from "./components/aboutme";
+function App() {
+    return (
+        <div>
+        <Navbar/>
+        <Intro/>
+        <Aboutme/>
+        </div>
+      
+      
+    );
+}
+
+export default App;
---
+++ src/components/aboutme.js
@@ -0,0 +1,7 @@
+function Aboutme() {
+    return (
+        <h1>Aboutme Component</h1>
+    )
+}
+
+export default Aboutme
